7b706e89c98f232f4a16f807a7b8aad301e259ce
HIGUCHI KASUMI <kasumia1103@gmail.com>
2025-06-20 10:23:38 +0900

    一括代入の実装

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -23,10 +23,11 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        $content = $request->input('content');
+        $inputs = $request->all();
 
-        $todo = new Todo(); 
-        $todo->content = $content;
+        $todo = new Todo();
+        $todo->user_id = Auth::id();
+        $todo->fill($inputs);
         $todo->save();
 
         return redirect()->route('todo.index');
