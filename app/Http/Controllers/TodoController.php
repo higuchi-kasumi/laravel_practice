e0a771c60c0164899f108e5028ca9955d47c986b
HIGUCHI KASUMI <kasumia1103@gmail.com>
2025-06-20 13:58:41 +0900

    LaravelテキストレビュTodo作成機能

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -26,7 +26,6 @@ class TodoController extends Controller
         $inputs = $request->all();
 
         $todo = new Todo();
-        $todo->user_id = Auth::id();
         $todo->fill($inputs);
         $todo->save();
 
