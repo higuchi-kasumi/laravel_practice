4127162b05ca3ad9965cd37cc7596c06a2c1e78d
HIGUCHI KASUMI <kasumia1103@gmail.com>
2025-06-24 07:19:37 +0900

    バリデーションの実装

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Todo;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
 
 class TodoController extends Controller
 {
@@ -27,7 +27,7 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();
 
@@ -53,15 +53,15 @@ class TodoController extends Controller
         return view('todo.edit', ['todo' => $todo]);
     }
 
-    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    public function update(TodoRequest $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
     {
         // TODO: リクエストされた値を取得
         $inputs = $request->all();
         
         // TODO: 更新対象のデータを取得
-        $todo = $this->todo->fill($inputs);
+        $todo = $this->todo->find($id);
         // TODO: 更新したい値の代入とUPDATE文の実行
-        $todo-> save();
+        $todo->fill($inputs)->save();
 
         return redirect()->route('todo.show', $todo->id);
 
