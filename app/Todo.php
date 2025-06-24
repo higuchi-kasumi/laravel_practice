66ba8752f102af0390bafde3d92b6500b99ce32e
HIGUCHI KASUMI <kasumia1103@gmail.com>
2025-06-24 10:50:37 +0900

    削除機能を論理削除に変更

--- app/Todo.php
+++ app/Todo.php
@@ -4,8 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Todo extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'todos';
 
     protected $fillable = [
